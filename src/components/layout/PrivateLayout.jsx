5e78f5b7161923fe8af8f79ea7ff24401da93a5e
Goloxina <francaudete1431@hotmail.com>
2026-02-04 12:38:13 +0100

    feat: integrar el menú superior en el layout privado

--- src/components/layout/PrivateLayout.jsx
+++ src/components/layout/PrivateLayout.jsx
@@ -1,12 +1,32 @@
+// src/components/layout/PrivateLayout.jsx
+
 import { Outlet } from "react-router-dom";
+import TopNav from "./TopNav";
+
+/*
+  Este layout envuelve TODAS las páginas privadas.
+
+  ¿Por qué existe?
+  - Para no repetir menú en cada página
+  - Para centralizar cosas comunes (menú, footer, etc.)
+*/
 
 export default function PrivateLayout() {
-    return (
-        <div>
-            <header>Private Header</header>
-            <main>
-                <Outlet />
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      {/* Menú superior visible solo para usuarios logueados */}
+      <TopNav />
+
+      {/* 
+        Outlet representa la página concreta:
+        - Dashboard
+        - Perfil
+        - Niveles
+        etc.
+      */}
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
