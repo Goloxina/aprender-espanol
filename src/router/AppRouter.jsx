d3539f515eb9417698d77cd83a1894e1a0864441
Goloxina <francaudete1431@hotmail.com>
2026-02-04 12:43:41 +0100

    feat: registrar rutas privadas y conectar el menú

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,48 +1,45 @@
 // src/router/AppRouter.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "../context/AuthContext";
 
-// Layouts
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import PublicLayout from "../components/layout/PublicLayout";
 import PrivateLayout from "../components/layout/PrivateLayout";
+import PrivateRoute from "./PrivateRoute";
 
 // Páginas públicas
 import Landing from "../pages/public/Landing";
 import Login from "../pages/public/Login";
-import Register from "../pages/public/Register";
 
-// Página privada
+// Páginas privadas
 import Dashboard from "../pages/private/Dashboard";
-
-// Protección de rutas
-import PrivateRoute from "./PrivateRoute";
+import Levels from "../pages/private/Levels";
+import Achievements from "../pages/private/Achievements";
+import Profile from "../pages/private/Profile";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      {/* AuthProvider DEBE estar dentro del Router */}
-      <AuthProvider>
-        <Routes>
-          {/* Rutas públicas */}
-          <Route element={<PublicLayout />}>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Route>
+      <Routes>
+        {/* Rutas públicas */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
 
-          {/* Rutas privadas */}
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <PrivateLayout />
-              </PrivateRoute>
-            }
-          >
-            <Route index element={<Dashboard />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
+        {/* Rutas privadas */}
+        <Route
+          element={
+            <PrivateRoute>
+              <PrivateLayout />
+            </PrivateRoute>
+          }
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/levels" element={<Levels />} />
+          <Route path="/achievements" element={<Achievements />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
